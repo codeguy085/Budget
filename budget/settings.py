2d5954bce9253a0eb18fd67da3b26e0c082a7a11
codeguy085 <bakhtiyarorujov@yandex.com>
2025-02-15 09:14:08 +0400

    debug false

--- budget/settings.py
+++ budget/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u@-(x1j+7i0ds6o697%7z%aam3t#5&!#*wyp1u@s8w(h7aly9)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
