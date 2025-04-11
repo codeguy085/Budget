e47de5152903d3546b55dea83d8c6dc1c12b8193
codeguy085 <bakhtiyarorujov@yandex.com>
2025-04-11 15:11:17 +0400

    admin improvements

--- loan/admin.py
+++ loan/admin.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 from .models import Loan, Payment
 # Register your models here.
 
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    readonly_fields = ['is_not_delayed', 'paid_at']
+    extra = 0
+
+
 @admin.register(Loan)
 class LoanAdmin(admin.ModelAdmin):
     list_display = ["customer", "amount", "term", "monthly_payment", "remaining_month", "paid_amount", "remaining_amount", "revenue", "start", "updated", "is_completed"]
     list_display_links = ["customer", "amount", "term", "monthly_payment", "remaining_month", "paid_amount", "remaining_amount", "revenue", "start", "updated", "is_completed"]
+    inlines = [PaymentInline]
+    search_fields = ['loan_id']
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ["loan", "payment", "customer", "paid_at", "is_not_delayed"]
     list_display_links = ["loan", "payment", "customer", "paid_at", "is_not_delayed"]
+    autocomplete_fields = ["loan"]
 
     @admin.display(description="Payment")
     def payment(self, obj):
--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Customer, CustomUser
+from loan.models import Loan
+from django.utils.html import format_html
+from django.urls import reverse
 # Register your models here.
+
+class LoanInline(admin.TabularInline):
+    model = Loan
+    readonly_fields = ['loan_link', 'amount', 'monthly_payment', 'term', 'is_completed']
+    fields = ['loan_link', 'amount', 'monthly_payment', 'term', 'is_completed']  # Replace loan_id with loan_link
+
+    def loan_link(self, obj):
+        if obj.pk:
+            url = reverse('admin:loan_loan_change', args=[obj.pk])
+            return format_html('<a href="{}">Loan #{}</a>', url, obj.loan_id)
+        return "-"
+    loan_link.short_description = "Loan"
+
 
 
 @admin.register(CustomUser)
@@ -13,3 +29,4 @@ class CustomUserAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["name", "surname", "active_montly_payment", "active_loans", "completed_loans", "all_loans", "total_remaining", "total_loan", "total_paid", "total_revenue", "created_at"]
     list_display_links = ["name", "surname", "active_montly_payment", "active_loans", "completed_loans", "all_loans", "total_remaining", "total_loan", "total_paid", "total_revenue", "created_at"]
+    inlines = [LoanInline]
