f8497a94e10e3b7dbfc7f4bdca99ac8e73f7dafd
bakhtiyar <bakhtiyarorujov@gmail.com>
2026-06-16 10:06:05 +0400

    edit loan date  fix

---
+++ loan/migrations/0012_merge_start_and_bigauto.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('loan', '0011_alter_investment_id_alter_transfer_id'),
+        ('loan', '0011_loan_start_editable'),
+    ]
+
+    operations = []
